1b6d7be42e8cbbbc1b5dbe1e056b48ffa126c8e0
Philip Washington Sorst <philip@sorst.net>
2017-07-13 13:37:00 +0200

    Moving static resources

--- src/main/java/net/dontdrinkandroot/gitki/wicket/headeritem/StyleCssHeaderItem.java
+++ src/main/java/net/dontdrinkandroot/gitki/wicket/headeritem/StyleCssHeaderItem.java
@@ -1,7 +1,9 @@
 package net.dontdrinkandroot.gitki.wicket.headeritem;
 
+import net.dontdrinkandroot.gitki.wicket.GitkiWebApplication;
 import org.apache.wicket.markup.head.CssReferenceHeaderItem;
-import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.request.Url;
+import org.apache.wicket.request.resource.UrlResourceReference;
 
 /**
  * @author Philip Washington Sorst <philip@sorst.net>
@@ -10,6 +12,13 @@ public class StyleCssHeaderItem extends CssReferenceHeaderItem
 {
     public StyleCssHeaderItem()
     {
-        super(new PackageResourceReference(StyleCssHeaderItem.class, "style.css"), null, null, null);
+        super(
+                new UrlResourceReference(
+                        Url.parse(GitkiWebApplication.get().getServletContext().getContextPath() + "/css/style.css")
+                ),
+                null,
+                null,
+                null
+        );
     }
 }
