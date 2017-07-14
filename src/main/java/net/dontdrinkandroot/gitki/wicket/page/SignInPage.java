85ff9d01700bb769eb28ccb7d8f47458b33d1f8d
Philip Washington Sorst <philip@sorst.net>
2017-07-14 13:37:00 +0200

    Redirecting from SignInPage if no admin user exists, closes #2

--- src/main/java/net/dontdrinkandroot/gitki/wicket/page/SignInPage.java
+++ src/main/java/net/dontdrinkandroot/gitki/wicket/page/SignInPage.java
@@ -1,12 +1,15 @@
 package net.dontdrinkandroot.gitki.wicket.page;
 
 import com.giffing.wicket.spring.boot.context.scan.WicketSignInPage;
+import net.dontdrinkandroot.gitki.service.user.UserService;
 import net.dontdrinkandroot.gitki.wicket.GitkiWebSession;
 import net.dontdrinkandroot.gitki.wicket.headeritem.StyleCssHeaderItem;
+import org.apache.wicket.RestartResponseAtInterceptPageException;
 import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
  * @author Philip Washington Sorst <philip@sorst.net>
@@ -14,9 +17,22 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 @WicketSignInPage
 public class SignInPage extends net.dontdrinkandroot.wicket.bootstrap.page.SignInPage
 {
+    @SpringBean
+    private UserService userService;
+
     public SignInPage(PageParameters parameters)
     {
         super(parameters);
+    }
+
+    @Override
+    protected void onInitialize()
+    {
+        if (!this.userService.hasAdminUser()) {
+            throw new RestartResponseAtInterceptPageException(FirstRunPage.class);
+        }
+
+        super.onInitialize();
     }
 
     @Override
