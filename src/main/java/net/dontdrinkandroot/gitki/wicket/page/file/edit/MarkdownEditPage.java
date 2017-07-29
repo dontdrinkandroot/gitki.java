fefba435e950f366023c06d950b1d587b08dfdfb
Philip Washington Sorst <philip@sorst.net>
2017-07-29 13:37:00 +0200

    i18n

--- src/main/java/net/dontdrinkandroot/gitki/wicket/page/file/edit/MarkdownEditPage.java
+++ src/main/java/net/dontdrinkandroot/gitki/wicket/page/file/edit/MarkdownEditPage.java
@@ -9,6 +9,7 @@ import net.dontdrinkandroot.gitki.wicket.util.PageParameterUtils;
 import net.dontdrinkandroot.wicket.bootstrap.component.button.AjaxSubmitButton;
 import net.dontdrinkandroot.wicket.bootstrap.component.form.formgroup.FormGroupInputText;
 import net.dontdrinkandroot.wicket.bootstrap.component.form.formgroup.FormGroupTextArea;
+import net.dontdrinkandroot.wicket.bootstrap.css.ButtonStyle;
 import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
@@ -19,6 +20,7 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.util.time.Duration;
@@ -84,7 +86,7 @@ public class MarkdownEditPage extends EditPage
         this.add(editForm);
 
         this.formGroupContent =
-                new FormGroupTextArea<>("content", Model.of("Content"), this.contentModel);
+                new FormGroupTextArea<>("content", new StringResourceModel("content"), this.contentModel);
         this.formGroupContent.setRows(20);
         this.formGroupContent.getFormComponent().add(new AjaxFormComponentUpdatingBehavior("input")
         {
@@ -108,26 +110,20 @@ public class MarkdownEditPage extends EditPage
         editForm.add(this.formGroupContent);
 
         FormGroupInputText formGroupCommitMessage =
-                new FormGroupInputText("commitMessage", Model.of("Commit message"), this.commitMessageModel);
+                new FormGroupInputText(
+                        "commitMessage",
+                        new StringResourceModel("commitmessage"),
+                        this.commitMessageModel
+                );
         formGroupCommitMessage.setRequired(true);
         editForm.add(formGroupCommitMessage);
 
-        AjaxSubmitButton submitButton = new AjaxSubmitButton("submit", Model.of("Save"))
+        AjaxSubmitButton saveAndBackButton = new AjaxSubmitButton("saveandback", new StringResourceModel("saveandback"))
         {
             @Override
             protected void onSubmit(AjaxRequestTarget target, Form<?> form)
             {
-                super.onSubmit(target, form);
-                try {
-                    MarkdownEditPage.this.gitService.addAndCommit(
-                            MarkdownEditPage.this.getModelObject(),
-                            MarkdownEditPage.this.contentModel.getObject(),
-                            GitkiWebSession.get().getUser(),
-                            MarkdownEditPage.this.commitMessageModel.getObject()
-                    );
-                } catch (IOException | GitAPIException e) {
-                    throw new WicketRuntimeException(e);
-                }
+                MarkdownEditPage.this.saveAndCommit();
             }
 
             @Override
@@ -140,7 +136,32 @@ public class MarkdownEditPage extends EditPage
                 );
             }
         };
-        editForm.add(submitButton);
+        editForm.add(saveAndBackButton);
+
+        AjaxSubmitButton saveButton = new AjaxSubmitButton("save", new StringResourceModel("save"))
+        {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form)
+            {
+                MarkdownEditPage.this.saveAndCommit();
+            }
+        };
+        saveButton.setButtonStyle(ButtonStyle.DEFAULT);
+        editForm.add(saveButton);
+    }
+
+    private void saveAndCommit()
+    {
+        try {
+            this.gitService.addAndCommit(
+                    this.getModelObject(),
+                    this.contentModel.getObject(),
+                    GitkiWebSession.get().getUser(),
+                    this.commitMessageModel.getObject()
+            );
+        } catch (IOException | GitAPIException e) {
+            throw new WicketRuntimeException(e);
+        }
     }
 
     //    @Override
